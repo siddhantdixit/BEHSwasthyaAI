d22c70f07ccf46a223d853be5f58ea728f52be19
SIDDHANT DIXIT <siddhant.dixit23@gmail.com>
2023-04-08 23:58:59 +0530

    Update blogController.js

---
+++ controllers/blogController.js
@@ -0,0 +1,79 @@
+const Blog = require('../models/blog');
+
+// Get all blogs
+const getAllBlogs = async (req, res, next) => {
+  try {
+    const blogs = await Blog.find();
+    res.status(200).json(blogs);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Get a blog by id
+const getBlogById = async (req, res, next) => {
+  try {
+    const blog = await Blog.findById(req.params.blogId);
+    if (!blog) {
+      return res.status(404).json({
+        message: 'Blog not found'
+      });
+    }
+    res.status(200).json(blog);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Create a new blog
+const createBlog = async (req, res, next) => {
+  try {
+    req.body.author = req.user._id; // Add the author id to the blog
+    const newBlog = new Blog(req.body);
+    const savedBlog = await newBlog.save();
+    res.status(201).json(savedBlog);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Update a blog by id
+const updateBlog = async (req, res, next) => {
+  try {
+    const updatedBlog = await Blog.findByIdAndUpdate(req.params.blogId, req.body, {
+      new: true,
+      runValidators: true
+    });
+    if (!updatedBlog) {
+      return res.status(404).json({
+        message: 'Blog not found'
+      });
+    }
+    res.status(200).json(updatedBlog);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Delete a blog by id
+const deleteBlog = async (req, res, next) => {
+  try {
+    const deletedBlog = await Blog.findByIdAndDelete(req.params.blogId);
+    if (!deletedBlog) {
+      return res.status(404).json({
+        message: 'Blog not found'
+      });
+    }
+    res.status(200).json(deletedBlog);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  getAllBlogs,
+  getBlogById,
+  createBlog,
+  updateBlog,
+  deleteBlog
+};
